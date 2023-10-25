3bd62571d85c175255fa50bc0cd4c929e041fe00
arthurgarcia <arthurgarcia@10.3.61.171>
2023-10-25 14:15:05 +0200

    la classe CelluleLumineuse a été ajoutée et est fonctionnelle

---
+++ LightOff_GARCIA_version_console/src/lightoff_garcia_version_console/LightOff_GARCIA_version_console.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_garcia_version_console;
+
+/**
+ *
+ * @author arthurgarcia
+ */
+public class LightOff_GARCIA_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       CelluleLumineuse cellule3 = new CelluleLumineuse();
+       System.out.println(cellule3.toString()); // Affichera "La cellule est éteinte"
+       CelluleLumineuse cellule4 = new CelluleLumineuse();
+       System.out.println(cellule4.toString());
+    }
+    
+}
